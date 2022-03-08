8218e2366ef1cbf4894df449dbb6b98568e1a60f
zl-mdc <1424084297@qq.com>
2022-03-09 00:00:39 +0800

    🎉 init: 小程序初始化

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { View } from "@tarojs/components";
+// import { useEnv, useNavigationBar, useModal, useToast } from "taro-hooks";
+
+import './index.scss'
+
+
+const Index = () => {
+  return (
+    <View className='wrapper'>
+       首页
+    </View>
+  );
+};
+
+export default Index;
